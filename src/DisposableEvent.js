140631ed32320b6e92e294133e8728a85b306952
Tuomas Hatakka <tuomas.hatakka@gmail.com>
2019-11-18 20:03:39 +0200

    Finish tests

--- src/DisposableEvent.js
+++ src/DisposableEvent.js
@@ -1,4 +1,5 @@
 import Disposable from './Disposable'
+import Emitter from "events"
 
 const missingArgumentError = (pre, name) => `${pre} the listened event must be provided for the DisposableEvent by passing a \`${name}\` property upon calling its constructor`
 const missingTypeError = missingArgumentError("The type of", "type")
@@ -29,8 +30,11 @@ export default class DisposableEvent extends Disposable {
       this.target.addEventListener(this.type, this.handler) :
       null
 
-    const unbind = () =>
+    const unbind = () => {
       this.target.removeEventListener(this.type, this.handler)
+      this.target = null
+      this.handler = null
+    }
 
     if (!handler)
       throw new Error(missingHandlerError)
@@ -39,7 +43,7 @@ export default class DisposableEvent extends Disposable {
       throw new Error(missingTypeError)
 
     if (!target)
-      target = document
+      target = getDefaultTarget()
 
     super(unbind)
 
@@ -51,4 +55,17 @@ export default class DisposableEvent extends Disposable {
 
   }
 
+}
+
+const emitter = typeof document === 'undefined' && new Emitter()
+
+
+function getDefaultTarget () {
+  if (typeof document !== 'undefined')
+    return document
+
+  return {
+    addEventListener:    emitter.addListener,
+    removeEventListener: emitter.removeListener,
+  }
 }
--- test.js
+++ test.js
@@ -1,14 +1,76 @@
 import test from 'ava'
-import { Disposable } from './dist'
+import { Disposable, DisposableCollection, DisposableEvent } from './dist'
 
-test("Disposable's disposal action gets executed when the disposable is disposed", t => {
+test("Disposable's disposal action gets executed exactly once when the disposable is disposed", t => {
 
   let n = 0  
   const fn = () => n++
   const disposable = new Disposable(fn)
   t.true(Disposable.isDisposable(disposable))
 
+  t.is(typeof disposable.disposalAction, 'function')
   disposable.dispose()
   t.is(disposable.disposalAction, null)
+
+  // derp
+  disposable.dispose()
+  disposable.dispose()
+  disposable.dispose()
+  disposable.dispose()
+  disposable.dispose()
+  disposable.dispose()
+
+  t.is(n, 1)
+})
+
+test("DisposableCollection's Disposables' disposal actions get executed when the disposable colection is disposed", t => {
+
+  let n = 0
+  const fn = () => n++
+
+  const disposable1 = new Disposable(fn)
+  const disposable2 = new Disposable(fn)
+  const disposable3 = new Disposable(fn)
+  const collection  = new DisposableCollection(disposable1, disposable2)
+
+  t.true(Disposable.isDisposable(collection))
+  t.is(collection.disposables.length, 2)
+
+  collection.add(disposable3)
+  t.is(collection.disposables.length, 3)
+  t.false(collection.disposed)
+
+  collection.dispose()
+  t.true(collection.disposed)
+
+  // derp vol 2
+  collection.dispose()
+  collection.dispose()
+  collection.dispose()
+  collection.dispose()
+  collection.dispose()
+  collection.dispose()
+
+  t.is(n, 3)
+})
+
+test("DisposableEvent", t => {
+
+  let n = 0
+  const fn = () => n++
+  let disposable = null
+
+  const create = (args) =>
+    disposable = new DisposableEvent(args)
+
+  t.throws(() => create())
+  t.throws(() => create({ handler: fn }))
+  t.notThrows(() => create({ handler: fn, type: 'click' }))
+
+  t.true(Disposable.isDisposable(disposable))
+
+  t.false(disposable.disposed)
+  disposable.dispose()
+  t.true(disposable.disposed)
 
 })
